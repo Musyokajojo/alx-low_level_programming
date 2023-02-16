7f8ccd7013df1fdc2f4a2475d72a8874daba02dc
musyokajojo <musyokajojo@gmail.com>
2023-02-16 13:31:09 -0800

    PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void) /*PRINTS THE WORDS IN BRACKETS*/
+/*
+ * main - prints "Programming is like building
+ * a multilingual puzzle", followed by a new line.
+ * Returns: Always 0.
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
