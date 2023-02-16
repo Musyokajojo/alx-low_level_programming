07fa3e826464c0235e519fa8cb7d3b31ff21876e
musyokajojo <musyokajojo@gmail.com>
2023-02-16 13:17:50 -0800

    PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-int main(void) 
-
+int main(void) /*PRINTS THE WORDS IN BRACKETS*/
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-        return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
