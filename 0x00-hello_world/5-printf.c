d55eb657ca188472d07b9af9e04cb50576b89c46
musyokajojo <musyokajojo@gmail.com>
2023-02-16 13:54:21 -0800

    PRINT F

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints "with proper grammar, but
- * the outcome is a piece of art," followed by a new line.
- * Return always 0.
+ * the outcome is a piece of art," followed by a new line
+ * Return: Always 0.
  *
  */
 int main(void)
